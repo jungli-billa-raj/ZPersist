fd7ab119a941766056e45fbc0b056c3de8e78943
Kumar Raj <rajbhkseries@gmail.com>
2026-02-03 21:02:39 +0530

    record_table struct in tdos_writer.zig

--- tdos_writer.zig
+++ tdos_writer.zig
@@ -9,16 +9,6 @@ const std = @import("std");
 // 16      8     Record table offset (u64)
 // 24      8     String blob offset (u64)
 // 32      8     File size (u64)
-//
-// [Record Table]
-// Each record (fixed 16 bytes):
-// 0       8     String offset (u64)
-// 8       4     String length (u32)
-// 12      4     Flags (done, deleted, etc.)
-//
-// [String Blob]
-// Raw UTF-8 bytes, tightly packed
-
 const header = struct {
     magic:[]const u8 = "TDOS", 
     version:u16 = 1,
@@ -28,3 +18,18 @@ const header = struct {
     string_blob_offset:u64,
     file_size:u64,
 };
+
+// [Record Table]
+// Each record (fixed 16 bytes):
+// 0       8     String offset (u64)
+// 8       4     String length (u32)
+// 12      4     Flags (done, deleted, etc.)
+const record_table = struct {
+    string_offset:u64,
+    string_length:u32,
+    flags:u32, // 0-done 1-deleted
+};
+
+// [String Blob]
+// Raw UTF-8 bytes, tightly packed
+// No struct needed
