e788b75dcdb80c2ca1f026404cf200090d6d2af2
Kumar Raj <rajbhkseries@gmail.com>
2026-02-05 20:32:05 +0530

    extern structs cannot contain fields of type'[]const u8'. note: slices have no guaranteed in-memory representation

---
+++ trial.zig
@@ -0,0 +1,24 @@
+
+const header = extern struct{
+    magic:[]const u8 = "TDOS", 
+    version:u16 = 1,
+    header_size:u16,
+    record_count:u64,
+    record_table_offset:u64,
+    // string_blob_offset:u64,
+    file_size:u64,
+};
+
+const std = @import("std");
+
+test "ss" {
+    const h = header{
+        .magic = "TDOS",
+        .version = 1, 
+        .header_size = 40,
+        .file_size = 0,
+        .record_count = 0,
+        .record_table_offset = 40,
+    };
+    std.debug.print("{b64}\n", .{std.mem.asBytes(&h)});
+}
